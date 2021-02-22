d6abb240c552ec01eb1158c3b8a6bbaf353eda03
vansh16100 <66217202+vansh16100@users.noreply.github.com>
2021-02-23 01:19:36 +0530

    Update and rename demoo.cpp to egg dropping puzzle.cpp

--- egg dropping puzzle.cpp
+++ egg dropping puzzle.cpp
@@ -6,38 +6,7 @@
 #define pic pair<int,char>
 using namespace std;
 using v_i=vector<int>;
-bool found(int arr[],int n,int k,int x) {
-  int tmp[n];
-  for(int i=0;i<n;i++) {
-    if(arr[i]<x) tmp[i] = -1;
-    else tmp[i] = 1;
-  }
-  for(int i=1;i<n;i++) tmp[i]=tmp[i-1]+tmp[i];
-  int mx=tmp[k-1];
-  int mn=INT_MAX;
-  for(int i=k;i<n;i++) {
-    mn=min(mn,tmp[i-k]);
-    mx=max(max(tmp[i],mx),tmp[i]-mn);
-  }
-  if(mx>0) return true;
-  else return false;
-}
-int maxMedian(int arr[],int n,int k) {
-//return 0;
-  int l=1;
-  int r=n;
-  int ans;
-  while(l<=r) {
-    int mid = (l+r)/2;
-    if(found(arr,n,k,mid)) {
-      ans=mid;
-      l=mid+1;
-    }else {
-      r=mid-1;
-    }
-  }
-  return ans;
-}
+
 int eggDropping(int e,int f) {
   int dp[e+1][f+1];
   for(int i=0;i<=f;i++) dp[0][i]=0,dp[1][i]=i;
