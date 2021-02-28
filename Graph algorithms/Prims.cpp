32729ece3764c9b81277e460d7865ecb4b8440d3
vansh16100 <66217202+vansh16100@users.noreply.github.com>
2021-02-28 23:40:00 +0530

    Update Prims.cpp

--- Graph algorithms/Prims.cpp
+++ Graph algorithms/Prims.cpp
@@ -1,119 +1,158 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class disjoint_set{
-    class node{
-        public:
-        int data;
-        int rank;
-        node* parent;
-        node(int data){
-            this->data=data;
-            rank=0;
-        }
-    };
-    map<int,node*> mp;
-    public:
-    disjoint_set(int n){
-        for(int i=0;i<n;i++){
-            mp[i]=new node(i);
-            mp[i]->parent=mp[i];
-        }
-    }
-    void Union(int d1,int d2){
-        node* parent1=find_set(mp[d1]);
-        node* parent2=find_set(mp[d2]);
-        
-        if(parent1==parent2)
-          return;
-        if(parent1->rank>=parent2->rank){
-            if(parent1->rank==parent2->rank)
-              parent1->rank=parent1->rank+1;
-            parent2->parent=parent1;
-        }
-        else parent1->parent=parent2;
-    }
-    
-    int find_set(int d){
-        node* f=find_set(mp[d]);
-        return f->data;
-    }
-    node* find_set(node* n){
-        if(n->parent==n)
-          return n;
-        n->parent=find_set(n->parent);
-    return n->parent;
-    }
-};
-class edge{
-    public:
-    int src,dest,wt;
-    edge(int wt,int src,int dest){
-        this->src=src;
-        this->dest=dest;
-        this->wt=wt;
-    }
-    egde(){}
+template<typename T>
+class node{
+		public:
+		T key;
+		int wt;
+	};
+template<typename T>
+class binary_heap{
+	public:
+	int Size;
+	int cap;
+	vector<node<T>> v;
+    map<T,int> position;
+	public:
+	binary_heap(int n){
+		cap=n;
+		Size=0;
+	}
+	void update_map(T n1,T n2,int pos1,int pos2){
+		position[n1]=pos1;
+		position[n2]=pos2;
+	}
+	void heapify(int i){
+		int l=2*i+1;
+		int r=2*i+2;
+		int mini=i;
+		if(l<Size&&v[l].wt<v[mini].wt)
+		  mini=l;
+		if(r<Size&&v[r].wt<v[mini].wt)
+		  mini=r;
+		 if(mini!=i)
+		 {swap(v[i],v[mini]);
+		   update_map(v[i].key,v[mini].key,i,mini);
+		  heapify(mini);
+		 }
+	}
+	
+   void add_ele(T j,int a){
+   	node<T> n;
+   	n.key=j;
+   	n.wt=a;
+   	v.push_back(n);
+   	int i=Size;
+   	position[n.key]=i;
+   	Size++;
+   	while(i>0){
+   		if(v[i].wt<v[(i-1)/2].wt)
+   		  swap(v[i],v[(i-1)/2] );
+   		  update_map(v[i].key,v[(i-1)/2].key,i,(i-1)/2);
+   		i=(i-1)/2;
+   	}
+   }
+   node<T> extract_min(){
+   	node<T> n=v[0];
+   	position.erase(v[0].key);
+   	position[v[Size-1].key]=0;
+   	swap(v[0],v[Size-1]);
+   	 
+   	Size--;
+   	v.pop_back();
+   	heapify(0);
+   	return n;
+   }
+   void decrease_key(T data,int weight){
+   	int pos=position[data];
+   	int i=pos;
+   	v[i].wt=weight;
+   	while(i>0){
+   			if(v[i].wt<v[(i-1)/2].wt)
+   		  swap(v[i],v[(i-1)/2]);
+   		  update_map(v[i].key,v[(i-1)/2].key,i,(i-1)/2);
+   		i=(i-1)/2;
+   	}
+   }
+   void print(){
+   	for(int i=0;i<v.size();i++){
+   		cout<<v[i].wt<<" "<<v[i].key<<endl;;
+   	}
+   }
+   int find_key(T key){
+   	if(position.find(key)==position.end())
+   	  return -1;
+   	  return position[key];
+   }
+   node<T> access_node(int pos){
+   	return v[pos];
+   }
 };
 class Graph{
-public:
-   vector<edge> edge_list;
-   void addEdge(int wt,int src,int des){
-    edge e(wt,src,des);
-    edge_list.push_back(e);
-   } 
+	int v;
+	map<int,list<pair<int,int>>> graph;
+	public:
+	Graph(int v){		this->v=v;
+	}
+	void addEdge(int u,int v,int w){
+		graph[u].push_back(make_pair(v,w));
+		graph[v].push_back(make_pair(u,w));
+	}
+	void print(){
+		for(auto adj:graph){
+			for(auto adjj:adj.second)
+			  cout<<adjj.second<<" "<<adj.first<<" "<<adjj.first<<endl;
+		}
+		cout<<endl;
+	}
+	void prims(int root){
+		map<pair<int,int>,int> ans;
+		map<int,int> parent;
+		binary_heap<int> heap(100);
+		for(auto it:graph){
+			if(it.first!=root)
+			heap.add_ele(it.first,INT_MAX);
+		}
+		heap.add_ele(root,0);
+        //cout<<endl;
+		parent[root]=root;
+		 while(heap.v.size()>0){
+		node<int> n=heap.extract_min();
+		ans[make_pair(n.key,parent[n.key])]=n.wt;
+		 	for(auto adj:graph[n.key]){
+		 			int pos=heap.find_key(adj.first);
+				if(pos!=-1){
+				node<int> n1=heap.access_node(pos);
+					if(adj.second<n1.wt){
+						heap.decrease_key(adj.first,adj.second);
+				       parent[adj.first]=n.key;
+					}
+				}
+		 	}      
+		 }
+		 for(auto it: ans){
+		 cout<<it.first.first<<" "<<it.first.second<<" "<<it.second<<endl;
+		 }
+	}
 };
-int myCompare(edge e1,edge e2){
-    return e1.wt < e2.wt;
-}
-int spanningTree(int V, Graph g) {
-       
-      sort((g.edge_list).begin(),(g.edge_list).end(),myCompare);
-      vector<pair<int,int>> v;
-      disjoint_set d(V);
-      int weight=0;
-      for(int i=0;i<g.edge_list.size();i++){
+int main() {
 
-          if(d.find_set(g.edge_list[i].src)!=d.find_set( g.edge_list[i].dest )){
-             {weight+=g.edge_list[i].wt;
-              d.Union(g.edge_list[i].src, g.edge_list[i].dest );
-              v.push_back(make_pair(g.edge_list[i].src, g.edge_list[i].dest));
-             }
-      }
-    }
-    for(auto it:v) {
-      cout<<setw(4)<<it.first<<" "<<setw(8)<<it.second<<"\n";
-    }
+     Graph g(9);
 
-     return weight;
-}
-
-int main(){
-  Graph g;
-// g.addEdge(1,7,6);
-// g.addEdge(2,8,2);
-// g.addEdge(2,6,5);
-// g.addEdge(4,0,1);
-// g.addEdge(4,2,5);
-// g.addEdge(6,8,6);
-// g.addEdge(7,2,3);
-// g.addEdge(7,7,8);
-// g.addEdge(8,0,7);
-// g.addEdge(8,1,2);
-// g.addEdge(9,3,4);
-// g.addEdge(10,5,4);
-// g.addEdge(11,1,7);
-// g.addEdge(14,3,5);
-int v,e;
-cin>>v>>e;
-int w,s,d;
-cout<<"vertices = "<<v<<"\n";
-cout<<"edges = "<<e<<"\n";
-cout<<setw(6)<<"source "<<setw(10)<<"destination"<<endl;
-for(int i=0;i<e;i++){
-  cin>>w>>s>>d;
-  g.addEdge(w,s,d);
-}
-  cout<<spanningTree(v,g);
-  return 0;
+	g.addEdge(7,6,1);
+	g.addEdge(8,2,2);
+	g.addEdge(6,5,2);
+	g.addEdge(0,1,4);
+	g.addEdge(2,5,4);
+	g.addEdge(8,6,6);
+	g.addEdge(2,3,7);
+	g.addEdge(7,8,7);
+	g.addEdge(0,7,8);
+	g.addEdge(1,2,8);
+	g.addEdge(3,4,9);
+	g.addEdge(5,4,10);
+	g.addEdge(1,7,11);
+	g.addEdge(3,5,14);
+	g.prims(0);
+	return 0;
 }
