1139f6be7b4b758e21c669c4e933933838b5fec2
vansh16100 <66217202+vansh16100@users.noreply.github.com>
2021-03-23 02:02:55 +0530

    Update bfsMatrix.cpp

--- Graph algorithms/bfsMatrix.cpp
+++ Graph algorithms/bfsMatrix.cpp
@@ -73,10 +73,10 @@ void solve() {
 
 }
 int main() {
-#ifndef ONLINE_JUDGE
-freopen("input.txt", "r", stdin);
-freopen("output.txt", "w", stdout);
-#endif
+// #ifndef ONLINE_JUDGE
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+// #endif
 solve();
 return 0;
  }
