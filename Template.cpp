46785bd7548e8300de7b95f1f678db3b02a0372f
vansh16100 <66217202+vansh16100@users.noreply.github.com>
2021-06-10 00:19:16 +0530

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pii pair<int,int>
+#define pic pair<int,char>
+using namespace std;
+using v_i=vector<int>;
+ 
+//debug
+#define deb(x) cout<<#x<<" = "<<x
+#define deb_s(x) cout<<#x<<" = "<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<" "<<#y<<"="<<y<<endl
+#define deb3(x, y,z) cout << #x << "=" << x << " " << #y << "=" << y<<" "<<#z<<"="<<z<<endl
+ 
+ inline void file() {
+     #ifndef ONLINE_JUDGE
+        freopen("e:/competitive/input.txt", "r", stdin);
+        freopen("e:/competitive/output.txt", "w", stdout);
+     #endif
+}
+ 
+void solve() {
+   
+}
+ 
+int main() {
+int t;
+cin>>t;
+while(t--){
+solve();
+  }
+return 0;
+ }
